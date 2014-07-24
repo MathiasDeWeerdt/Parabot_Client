851bcfc842d8e40f5d39827a6ada60a27f867712
Mathias <mathias.dw@hotmail.com>
2014-07-24 15:57:46 +0200

    Initial commit
    Initial commit

---
+++ Cursor.java
@@ -0,0 +1,16 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Cursor extends client implements ActionListener {
+	public static Cursor frame;
+
+	public void actionPerformed(ActionEvent evt) {
+		String cmd = evt.getActionCommand();
+		try {
+			if (cmd != null) {
+
+			}
+		} catch (Exception e) {
+		}
+	}
+}
---
+++ SoundProvider.java
@@ -0,0 +1,57 @@
+// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.kpdus.com/jad.html
+// Decompiler options: packimports(3) 
+// Source File Name:   SoundProvider.java
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+
+public class SoundProvider {
+
+	protected SoundProvider() {
+		try {
+			midiSequencer = MidiSystem.getSequencer();
+		} catch (MidiUnavailableException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static SoundProvider getSingleton() {
+		return instance = instance != null ? instance : new SoundProvider();
+	}
+
+	public void playMIDI(byte data[]) {
+		if (!midiSequencer.isOpen())
+			try {
+				midiSequencer.open();
+			} catch (MidiUnavailableException e1) {
+			}
+		if (midiSequencer.isRunning())
+			midiSequencer.stop();
+		try {
+			midiSequence = MidiSystem.getSequence(new BufferedInputStream(
+					new ByteArrayInputStream(data)));
+			midiSequencer.setLoopCount(-1);
+			midiSequencer.setSequence(midiSequence);
+		} catch (InvalidMidiDataException e) {
+		} catch (IOException e) {
+		}
+		midiSequencer.start();
+	}
+
+	public void stopMIDI() {
+		midiSequencer.stop();
+	}
+
+	public Sequencer midiSequencer;
+	private Sequence midiSequence;
+	private static SoundProvider instance = null;
+
+}
